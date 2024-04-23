b06fe8e611472f91bb91496dd344df036fb37ab1
OneEuroQuestion <mike@oneeuroquestion.com>
2024-04-23 12:26:48 +0200

    style

--- src/components/BlockWithDateCard.tsx
+++ src/components/BlockWithDateCard.tsx
@@ -19,14 +19,14 @@ const BlockWithDateCard: FC<BlockWithDateCardProps> = ({ blockData }) => (
         <Flex gap="3" align="center">
             <Box style={{ paddingLeft: '5px' }}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Image
+                        width={40}
+                        height={40}
+                        src="/block-line.svg"
+                        alt="export-csv"
+                        style={{ display: 'inline-block', verticalAlign: 'middle', margin: '-15px', paddingBottom: '2px' }}
+                    />
                     <Text size="2" weight="bold">
-                        <Image
-                            width={40}
-                            height={40}
-                            src="/block-line.svg"
-                            alt="export-csv"
-                            style={{ display: 'inline-block', verticalAlign: 'middle', margin: '-15px', paddingBottom: '2px' }}
-                        />
                         <span style={{ marginLeft: '10px', whiteSpace: 'nowrap' }}>Block {blockData.height}</span>
                     </Text>
                     <Text size="1" color="gray" style={{ marginLeft: '6px', whiteSpace: 'nowrap' }}>
