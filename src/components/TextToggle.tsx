b2d998b7d7df1da0d23037417b72506bcbb34ecb
OneEuroQuestion <mike@oneeuroquestion.com>
2024-05-14 17:37:42 +0200

    prints & css

---
+++ src/components/TextToggle.tsx
@@ -0,0 +1,17 @@
+// src/components/TextToggle.tsx
+
+import React from 'react';
+import Toggle from 'rsuite/Toggle';
+
+interface TextToggleProps {
+    openText: string;
+    closeText: string;
+    onChange: (checked: boolean, event: React.ChangeEvent<HTMLInputElement>) => void;
+    style?: React.CSSProperties;
+}
+
+const TextToggle: React.FC<TextToggleProps> = ({ openText, closeText, onChange, style }) => (
+    <Toggle size="xs" checkedChildren={openText} unCheckedChildren={closeText} onChange={onChange} style={style} />
+);
+
+export default TextToggle;
