86954f7f097a09613d427b11a971c8c15a2362d8
OneEuroQuestion <mike@oneeuroquestion.com>
2024-08-28 13:20:23 +0200

    not needed css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Theme
           appearance="dark"
