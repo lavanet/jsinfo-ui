33de3ad4fc6130a26438ed53f0a06ec31607a6e1
OneEuroQuestion <mike@oneeuroquestion.com>
2024-10-02 17:15:35 +0200

    fix

--- src/app/consumer/[lavaid]/_components/ConsumerConflictsTab.tsx
+++ src/app/consumer/[lavaid]/_components/ConsumerConflictsTab.tsx
@@ -14,7 +14,8 @@ interface ConsumerConflictsTableProps {
 const ConsumerConflictsTable: React.FC<ConsumerConflictsTableProps> = ({ lavaid }) => {
     const { data: consumer, loading, error } = useJsinfobeFetch('consumerConflicts/' + lavaid);
 
-    if (error) return <ErrorDisplay message={error} />;
+    // this loads together with the first tab 
+    if (error) return null; //return <ErrorDisplay message={error} />;
     if (loading) return null; //<LoadingIndicator loadingText={`Loading ${lavaid} conflicts data`} greyText={`${lavaid} conflicts`} />;
 
     return (
